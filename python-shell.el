5761865d4a5b3aa48f43d17753efbbdaf90796be
fj <fj@Viajero>
2022-08-18 11:23:33 +0900

    Add python-shell.el

---
+++ python-shell.el
@@ -0,0 +1,42 @@
+;;; python-shell.el --- python shell sender
+;;; Commentary:
+;;; Code:
+(require 'python)
+
+;;; interactive
+;;;###autoload
+(defun my-python-shell-send-line ()
+  "Send python shell pointd line."
+  (interactive)
+  (let ((string (replace-regexp-in-string
+                 "^[ \t]+"
+                 ""
+                 (thing-at-point 'line t)))
+        (process (python-shell-get-process)))
+    (my-python-shell-send-string string process)))
+
+;;;###autoload
+(defun my-python-shell-send-region (s e)
+  "Send python shell region.
+S region start point
+E region end point"
+  (interactive "r")
+  (let ((region-string (buffer-substring-no-properties s e))
+        (process (python-shell-get-process)))
+    (mapc (lambda (string)
+            (my-python-shell-send-string string process)
+            ;; Wait for python side standard output.
+            (sleep-for 0.01))
+          (split-string region-string "[\n\r]"))))
+
+(defun my-python-shell-send-string (string process)
+    "Send string to the process.
+STRING substring up to newline is sent.
+PROCESS process object"
+    (with-current-buffer (process-buffer process)
+    (goto-char (point-max))
+    (insert (car (split-string string "[\n\r]")))
+    (comint-send-input)
+    (goto-char (point-max))))
+
+;;; python-shell.el ends here

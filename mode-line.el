a0a635b946fd917dec077aa1a862835b3f28dcee
fujimotok <fujimotok@users.noreply.github.com>
2023-02-21 00:27:21 +0900

    fix mode-line icon align.

--- mode-line.el
+++ mode-line.el
@@ -18,26 +18,26 @@ FUNC is function that returns new property value."
 (defconst mml-mode-name
   '(:eval
     (-> (all-the-icons--icon-info-for-buffer)
-        (over-propertize 'display (lambda (x) '(raize 0.0))))))
+        (over-propertize 'display (lambda (x) '(raise -0.1))))))
 
 (defconst mml-branch-icon
   (-> (all-the-icons-octicon "git-branch")
-      (over-propertize 'display (lambda (x) '(raize 0.0)))
+      (over-propertize 'display (lambda (x) '(raise -0.0)))
       (over-propertize 'face (lambda (x) (plist-put x :foreground "green")))))
 
 (defconst mml-error-icon
   (-> (all-the-icons-faicon "times-circle")
-      (over-propertize 'display (lambda (x) '(raize 0.0)))
+      (over-propertize 'display (lambda (x) '(raise -0.1)))
       (over-propertize 'face (lambda (x) (plist-put x :foreground "red")))))
 
 (defconst mml-warning-icon
   (-> (all-the-icons-faicon "exclamation-triangle")
-      (over-propertize 'display (lambda (x) '(raize 0.0)))
+      (over-propertize 'display (lambda (x) '(raise -0.1)))
       (over-propertize 'face (lambda (x) (plist-put x :foreground "yellow")))))
 
 (defconst mml-info-icon
   (-> (all-the-icons-faicon "info-circle")
-      (over-propertize 'display (lambda (x) '(raize 0.0)))
+      (over-propertize 'display (lambda (x) '(raise -0.1)))
       (over-propertize 'face (lambda (x) (plist-put x :foreground "cyan")))))
 
 (defun flymake-count (type)

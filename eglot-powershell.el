29b89ccca11a4cc02bf17787d58e33aa66e9d369
fujimotok <fujimotok@users.noreply.github.com>
2024-02-04 23:13:27 +0900

    Add eglot-powershell.

---
+++ eglot-powershell.el
@@ -0,0 +1,60 @@
+;;; eglot-powershell.el --- eglot powershell setting
+;;; Commentary:
+;;; Code:
+(require 'eglot)
+
+(defgroup eglot-powershell nil
+  "Eglot support for PowerShell, using the PowerShellEditorServices.")
+
+(defcustom eglot-powershell-pwsh-exe (or (executable-find "pwsh") (executable-find "powershell"))
+  "PowerShell executable."
+  :type 'string
+  :group 'eglot-powershell)
+
+(defcustom eglot-powershell-language-server-dir "~/.emacs.d/.cache/lsp/pwsh"
+  "Language server install dir."
+  :type 'string
+  :group 'eglot-powershell)
+
+(defcustom eglot-powershell-lsp-command
+  `(,eglot-powershell-pwsh-exe
+   "-NoProfile" "-NonInteractive" "-NoLogo"
+   ,@(if (eq system-type 'windows-nt) '("-ExecutionPolicy" "Bypass"))
+   "-OutputFormat" "Text"
+   "-File"
+   ,(expand-file-name "PowerShellEditorServices/Start-EditorServices.ps1" eglot-powershell-language-server-dir)
+   "-HostName" "\"Emacs Host\""
+   "-HostProfileId" "'Emacs.LSP'"
+   "-HostVersion" "8.0.1"
+   "-LogPath" ,(expand-file-name "emacs-powershell.log" eglot-powershell-language-server-dir)
+   "-LogLevel" "Normal"
+   "-SessionDetailsPath" ,(expand-file-name (format "PSES-VSCode-%d" (emacs-pid))
+                                            eglot-powershell-language-server-dir)
+   ;; "-AdditionalModules" "@('PowerShellEditorServices.VSCode')"
+   "-Stdio"
+   "-BundledModulesPath" ,(expand-file-name eglot-powershell-language-server-dir)
+   "-FeatureFlags" "@()")
+  "Start lsp server command."
+  :type 'string
+  :group 'eglot-powershell)
+
+(with-eval-after-load 'eglot
+  (add-to-list
+   'eglot-server-programs
+   `(powershell-mode . ,eglot-powershell-lsp-command)))
+
+;;; interactive
+;;;###autoload
+(defun eglot-powershell-install-server ()
+  (interactive)
+  (make-directory eglot-powershell-language-server-dir t)
+  (async-shell-command (format "%s -Command \"Invoke-WebRequest %s -OutFile %s; Expand-Archive -Path %s -DestinationPath %s; Remove-Item -Path %s\""
+                         eglot-powershell-pwsh-exe
+                         "https://github.com/PowerShell/PowerShellEditorServices/releases/latest/download/PowerShellEditorServices.zip"
+                         "PowerShellEditorServices.zip"
+                         "PowerShellEditorServices.zip"
+                         (expand-file-name eglot-powershell-language-server-dir)
+                         "PowerShellEditorServices.zip")))
+
+(provide 'eglot-powershell)
+;;; eglot-powershell.el ends here
